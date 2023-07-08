0d53746fa376711df73c970bb61fae9457efeb94
«Domore-kr» <placebo9132@gmail.com>
2023-07-08 14:52:12 +0500

    Реализована книга

--- src/components/book/Book.tsx
+++ src/components/book/Book.tsx
@@ -2,36 +2,72 @@ import React, { useState } from 'react';
 import './Book.css';
 
 const Book: React.FC = () => {
-    const [currentPage, setCurrentPage] = useState<number>(0);
+    const [isOpen, setIsOpen] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const openBook = () => {
+        setIsOpen(true);
+    };
+
+    const closeBook = () => {
+        setIsOpen(false);
+    };
 
     const nextPage = () => {
-        setCurrentPage((prevPage) => prevPage + 1);
+        if (currentPage < 5) {
+            setCurrentPage(currentPage + 2);
+        }
     };
 
     const prevPage = () => {
-        setCurrentPage((prevPage) => prevPage - 1);
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 2);
+        }
     };
 
     return (
         <div className="book-container">
-            <div className="book">
-                <div className={`page page-left ${currentPage === 0 ? 'current-page' : ''}`}>
+            <div className={`book ${isOpen ? 'open' : ''}`}>
+                <div className="cover" onClick={openBook}>
+                    <h2>Cover</h2>
+                </div>
+                <div className={`page page-left ${isOpen && currentPage === 1 ? 'active' : ''}`}>
                     <h2>Page 1</h2>
                 </div>
-                <div className={`page page-right ${currentPage === 1 ? 'current-page' : ''}`}>
+                <div className={`page page-right ${isOpen && currentPage === 1 ? 'active' : ''}`}>
                     <h2>Page 2</h2>
                 </div>
-                <div className={`page page-left ${currentPage === 2 ? 'current-page' : ''}`}>
+                <div className={`page page-left ${isOpen && currentPage === 3 ? 'active' : ''}`}>
                     <h2>Page 3</h2>
+                </div>
+                <div className={`page page-right ${isOpen && currentPage === 3 ? 'active' : ''}`}>
+                    <h2>Page 4</h2>
+                </div>
+                <div className={`page page-left ${isOpen && currentPage === 5 ? 'active' : ''}`}>
+                    <h2>Page 5</h2>
+                </div>
+                <div className={`page page-right ${isOpen && currentPage === 5 ? 'active' : ''}`}>
+                    <h2>Page 6</h2>
                 </div>
             </div>
             <div className="navigation-buttons">
-                <button onClick={prevPage} disabled={currentPage === 0}>
-                    Previous Page
-                </button>
-                <button onClick={nextPage} disabled={currentPage === 2}>
-                    Next Page
-                </button>
+                {isOpen ? (
+                    <>
+                        <button className="prev-button" onClick={prevPage} disabled={currentPage <= 1}>
+                            Previous Page
+                        </button>
+                        <button className="next-button" onClick={nextPage} disabled={currentPage >= 5}>
+                            Next Page
+                        </button>
+                        <button className="close-button" onClick={closeBook}>
+                            Close Book
+                        </button>
+                    </>
+                ) : (
+                    <button className="open-button" onClick={openBook}>
+                        Open Book
+                    </button>
+                )}
             </div>
         </div>
     );
